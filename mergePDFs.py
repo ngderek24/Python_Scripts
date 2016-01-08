0ffdcd55e9bce359eb99939624370973990f4aea
Derek Nguyen <tincan24@gmail.com>
2016-01-07 21:36:41 -0800

    Create mergePDFs.py

---
+++ mergePDFs.py
@@ -0,0 +1,31 @@
+#! python3
+# mergePDFs.py Combines all the PDFs in the current direction into one PDF
+# inspired by "Automate the Boring Stuff"
+
+import PyPDF2, os
+
+# get all files with pdf extension
+pdfFiles = []
+for filename in os.listdir('.'):
+    if filename.endswith('.pdf'):
+        pdfFiles.append(filename)
+
+# sort names of PDF files
+pdfFiles.sort(key=str.lower)
+
+pdfWriter = PyPDF2.PdfFileWriter()
+
+# create reader for each file
+for filename in pdfFiles:
+    fileObj = open(filename, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(fileObj)
+
+    # add all pages of pdf
+    for pgNum in range (0, pdfReader.numPages):
+        pgObj = pdfReader.getPage(pgNum)
+        pdfWriter.addPage(pgObj)
+
+# save the merged pdf
+mergedPdf = open('merged.pdf', 'wb')
+pdfWriter.write(mergedPdf)
+mergedPdf.close()
